a08c23ff7dd932c32b1da1c1a951908e59811208
Max Base <MaxBaseCode@Gmail.Com>
2019-03-22 23:06:19 +0430

    Update main.go

--- main.go
+++ main.go
@@ -1,2 +1,10 @@
-/*
-*/
+/**
+*
+* @Name : DiskAnalyst
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-03-19
+* @Released under : https://github.com/BaseMax/DiskAnalyst/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/DiskAnalyst
+*
+**/
